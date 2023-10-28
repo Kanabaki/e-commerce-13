4493bdae6ab060ab10eadf63cc0ac8acb6fe827f
Kanabaki <Kagomekanabaki@gmail.com>
2023-10-28 11:06:45 -0500

    model associations updated

--- models/index.js
+++ models/index.js
@@ -16,10 +16,12 @@ Category.hasMany(Product,{
 // Products belongToMany Tags (through ProductTag)
 // so about the foreign keys below ...the associations have the Junction Table ProductTag,
 // so is there a need to add foreign keys if the ProductTag already has the keys? But if I go by this
-// logic, then I don't need to do so for the associations above. 
-Product.belongsToMany(Tag,{ through: ProductTag, foreignKey: 'product_id'}) 
+// logic, then I don't need to do so for the associations above. It looks like since we're using a 
+// junction table, we don't need to give it the foreign keys
+// ref https://www.youtube.com/watch?v=HJGWu0cZUe8&t=3074s&ab_channel=WittCode
+Product.belongsToMany(Tag,{ through: ProductTag, }) // foreignKey: 'product_id'
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product,{ through: ProductTag, foreignKey: 'tag_id'}) 
+Tag.belongsToMany(Product,{ through: ProductTag, }) // foreignKey: 'tag_id'
 
 module.exports = {
   Product,
